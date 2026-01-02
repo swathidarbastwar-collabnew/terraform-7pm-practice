39a22b1d70e046edba228058d86a659932ba9208
vishal <swathi.darbastwar@gmail.com>
2026-01-02 16:54:01 +0530

    day-3

---
+++ Day-3-state-backup-from-local/main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "ec2" {
+    instance_type = "t3.micro"
+    ami = "ami-068c0051b15cdb816"
+    tags = {
+      name = "Dev"  
+    }
+}
+
+resource "aws_s3_bucket" "s3bucket1" {
+    bucket = "sghdomudbsldhdse"
+  
+}
