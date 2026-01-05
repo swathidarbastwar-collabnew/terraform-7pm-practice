506798b5c07506a40ad443b9f295ac8575ec626b
vishal <swathi.darbastwar@gmail.com>
2026-01-05 16:38:24 +0530

    day-4

--- Day-4-Remote-state/main.tf
+++ Day-4-Remote-state/main.tf
@@ -1,13 +1,7 @@
-
-resource "aws_s3_bucket" "s3bucket1" {
-    bucket = "sghdomudbsldhdse"
-  
-}
-
 resource "aws_instance" "ec2" {
     instance_type = "t3.micro"
     ami = "ami-068c0051b15cdb816"
     tags = {
-      name = "dev1"  
+      Name = "Dev"  
     }
 }
---
+++ Day-4-state/main.tf
@@ -0,0 +1,4 @@
+resource "aws_s3_bucket" "s3bucket3" {
+    bucket = "opijsdertyuionmdchgst"
+  
+}
