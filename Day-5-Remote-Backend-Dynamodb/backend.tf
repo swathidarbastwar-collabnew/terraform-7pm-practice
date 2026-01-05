57f1fac53fdd56b59a730d64ccfa57f6751b8bd4
vishal <swathi.darbastwar@gmail.com>
2026-01-05 18:07:58 +0530

    day-5

---
+++ Day-5-Remote-Backend-Dynamodb/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket = "sghdomudbsldhdse"
+    key    = "day-5/terraform.tfstate"
+    region = "us-east-1"
+    #use_lockfile = true
+    dynamodb_table = "vishal"
+  }
+}
